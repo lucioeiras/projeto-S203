497874f346aa8e487f88d4f65d2beecdffdaaaf2
Diego Eiras <eiras.lucio@gmail.com>
2024-04-30 20:43:58 -0300

    :fire: FEAT: Adiciona middleware para validar token

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import jwt from "jsonwebtoken";
 
 import userRouter from "./User.js";
 import movieRouter from "./Movie.js";
@@ -6,6 +7,24 @@ import movieRouter from "./Movie.js";
 const routes = Router();
 
 routes.use(userRouter);
+
+routes.use((request, response, next) => {
+  const authHeader = request.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return response.status(401).json({ message: "Token not provided" });
+  }
+
+  try {
+    jwt.verify(token, process.env.PRIVATE_KEY);
+  } catch (err) {
+    return response.status(401).json({ message: "Token invalid" });
+  }
+
+  next();
+});
+
 routes.use(movieRouter);
 
 export default routes;
